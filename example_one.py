3a546f649e352ca0a6d95b7441b85ab264c516f6
harold1003 <haroldesneider45@gmail.com>
2023-02-20 07:49:02 -0500

    tipos_variables

---
+++ example_one.py
@@ -0,0 +1,34 @@
+""""
+name = "jesus maria gomez"
+print("hello",name)
+
+# ejemplo de casting (convertir una variable de un tipo de dato a otro)
+variable = 5 
+variable_uno = float(variable)
+variable_dos = str(variable)
+
+#print(variable, variable_uno)
+print(variable,variable_uno,variable_dos)
+
+#print(variable, variable_uno)
+print(variable, variable_uno, variable_dos)
+print(type(variable_dos))
+print(type(variable_uno))
+#print(sumar_datos)
+
+"""
+#tipos de datos 
+variable1 = "Hola"
+variable2 = 20
+variable3 = 20.8 
+varibale4 = 1j
+variable5 = ["cantar", "jugar", "estudiar"] #list  
+variable6 = ("cantar", "jugar", "estudiar") #tupla
+variable7 = {"color": "rojo", "velocidad": 300, "modelo": "GTRR36"} #dict
+#print(variable5)
+#print(variable6)
+#print(carro) 
+
+print(variable5)
+print(variable6)
+print(variable7)

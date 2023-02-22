495d9c410b8e46b5f02e85aee5ac9c7b9d02063a
harold1003 <haroldesneider45@gmail.com>
2023-02-22 09:07:10 -0500

    actividad_wilson

---
+++ actividad.py
@@ -0,0 +1,87 @@
+"""
+#ejerciciouno
+numero_tarjeta = 10554
+imprimir(escribir(numero_tarjeta))
+saldo = 18.3500
+imprimir (escribir (saldo))
+cantidad_retirar = 10.000
+imprimir(tipo(cantidad_retirar))
+contraseña_tarjeta = 8550
+imprimir (escribir(contraseña_tarjeta))
+confirmar_retiro = "Si confirmo"
+imprimir (escribir(confirmar_retiro))
+"""
+""""
+nombre_usuario = "josezuleta"
+imprimir(tipo(nombre_usuario))
+fecha_hora = "18/5/2020"
+imprimir(tipo(fecha_hora))
+mensajes = "Hola buenas tardes como te va"
+imprimir(escribir(mensajes))
+usuario_linea = "esteban"
+imprimir(tipo(usuario_linea))
+"""
+
+""""
+numero_telefono = 3206479890
+imprimir(tipo(numero_telefono))
+señal = "disponible"
+imprimir(tipo(señal))
+saldo= 25.000
+imprimir(escribir(saldo))
+"""
+
+"""
+cedula=104845644
+imprimir(escribir(cedula))
+contraseña=1556
+imprimir(escribir(contraseña))
+saldo=23.000
+imprimir(escribir(saldo))
+confirmar_pago="confirmar pago"
+imprimir(escribir(confirmar_pago))
+recibo = "total 50.000"
+imprimir(escribir(recibo))
+transaccion_finalizada="transaccion finalizada"
+imprimir(tipo(transaccion_finalizada))
+"""
+"""""
+forma_lavado="lavadora"
+imprimir(tipo(forma_lavado))
+tiempo= "8:20"
+imprimir(escribir(tiempo))
+cantidad_agua= "200ml"
+imprimir(tipo(cantidad_agua))
+cantidad_detergente="10ml"
+imprimir(tipo(cantidad_detergente))
+cantidad_suavitel="60ml"
+imprimir(tipo(cantidad_suavitel))
+"""
+articulos = {"zapatos" : 350000, "tenis" : 280000, "camisetas" : 175000, "jeans" : 200000}
+
+zapatos = 350000
+tenis = 280000
+camisetas = 175000
+jeans = 200000
+
+jeanspor = jeans * 0.062
+jean = jeans + jeanspor
+
+zapatos_por = zapatos * 0.008
+zapatos = zapatos - zapatos_por
+
+costototal = zapatos+tenis+camisetas+jeans
+
+promedio_venta = zapatos + tenis + camisetas + jeans / 4
+
+#print(articulos,total,promedioventa) sin espacios.
+
+
+print ("articulos/precio", articulos)
+print("costototalventa", costototal)
+print( "promedioventa", promedio_venta)
+
+print("precio nuevo jeans(suma)" , jean , "precio nuevo zapatos (resta)" , zapatos)
+
+#actividad harold
+
